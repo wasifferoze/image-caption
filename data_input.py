6f363f2f44674978f84cac248f385be1115c1924
Wasif Feroze <wasif_feroze@yahoo.com>
2018-01-30 22:17:01 +0800

    add TODO

--- data_input.py
+++ data_input.py
@@ -3,7 +3,9 @@ this code is for input a data from given image folder
 it will pre process these images for nn models (VGG model)
 return them in dictionary on their ids
 """
-from os import  listdir
+#TODO: add image pre processing support for multiple model networks
+
+from os import listdir
 from keras.preprocessing.image import load_img, img_to_array
 from keras.applications.vgg16 import preprocess_input
 
@@ -11,7 +13,7 @@ def load_image_data(dir_name):
     images_dict = dict()
     for name in listdir(dir_name):
         file_name = dir_name + '/' + name
-        img = load_img(file_name, target_size=(224, 224))
+        img = load_img(file_name, target_size=(224, 224)) #TODO:change hardcode
         img = img_to_array(img)
         img_reshape = img.reshape((1, img.shape[0], img.shape[1], img.shape[2]))
         image = preprocess_input(img_reshape)
