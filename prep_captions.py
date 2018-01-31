fbd39115954d060c3dd1946b3d7f07cf7f806eb2
Wasif Feroze <wasif_feroze@yahoo.com>
2018-01-31 19:46:59 +0800

    prepare description

---
+++ prep_captions.py
@@ -0,0 +1,52 @@
+import string
+
+
+# load doc files
+def load_doc(file_name):
+    file = open(file_name, 'r')
+    txt = file.read()
+    file.close()
+    return txt
+
+
+# images description extraction
+def load_desc(desc_doc):
+    mapp_dict = dict()
+    for line in desc_doc.split('\n'):
+        tokens = line.split()
+        if len(line) < 2:
+            continue
+        img_id, img_desc = tokens[0], tokens[1:]
+        img_id = img_id.split('.')[0]
+        img_desc = ' '.join(img_desc)
+        if img_id not in mapp_dict:
+            mapp_dict[img_id] = img_desc
+    return mapp_dict
+
+
+# for cleaning description text
+def clean_descriptions(descriptions):
+    # prepare translation table for removing punctuation
+    table = str.maketrans('', '', string.punctuation)
+    for key, desc in descriptions.items():
+        # tokenize
+        desc = desc.split()
+        # convert to lower case
+        desc = [word.lower() for word in desc]
+        # remove punctuation from each token
+        desc = [w.translate(table) for w in desc]
+        # remove hanging 's' and 'a'
+        desc = [word for word in desc if len(word) > 1]
+        # store as string
+        descriptions[key] = ' '.join(desc)
+
+
+# save descriptions to file, one per line
+def save_doc(descriptions, filename):
+    lines = list()
+    for key, desc in descriptions.items():
+        lines.append(key + ' ' + desc)
+    data = '\n'.join(lines)
+    file = open(filename, 'w')
+    file.write(data)
+    file.close()
